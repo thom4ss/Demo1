7372e79634d8daa4f265a2f4ddf2d0aa6c46d2e3
thom4ss <thomas09andre2019@gmail.com>
2023-03-09 09:55:33 +0700

    menambah file java

---
+++ BelajarJava11.java
@@ -0,0 +1,36 @@
+package WEEK11;
+
+import java.util.*;
+
+public class BelajarJava11 {
+    public static void main(String[] args) {
+        ArrayList<Mahasiswa> mhs = new ArrayList<>();
+        addd(mhs);
+        //sortt(mhs);
+        printt(mhs);
+    }
+    public static void printt(ArrayList<Mahasiswa> mhs){
+
+    }
+    public static void shortt(ArrayList<Mahasiswa> mhs){
+
+    }
+    public static void addd(ArrayList<Mahasiswa> mhs){
+        mhs.add(new Mahasiswa("001","Dani",5.5));
+        mhs.add(new Mahasiswa("001","Dani",9.5));
+        mhs.add(new Mahasiswa("001","Dani",2.5));
+    }
+}
+class Mahasiswa{
+    private String NIM,Nama;
+    private double IPK;
+
+    public Mahasiswa(String NIM, String Nama, double IPK){
+        this.NIM = NIM;
+        this.Nama = Nama;
+        this.IPK = IPK;
+    }
+    public double getData(){
+        return IPK;
+    }
+}
